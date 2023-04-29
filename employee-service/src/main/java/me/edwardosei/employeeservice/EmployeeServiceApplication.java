199b83086967dc03d24512071f218c0e50a0ab3a
nana-yaw <edward.osei.nyarko@gmail.com>
2023-04-29 15:35:32 +0000

    employee model-mapper dependency

--- employee-service/src/main/java/me/edwardosei/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/me/edwardosei/employeeservice/EmployeeServiceApplication.java
@@ -1,10 +1,14 @@
 package me.edwardosei.employeeservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
--- employee-service/src/main/java/me/edwardosei/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/me/edwardosei/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import me.edwardosei.employeeservice.dto.EmployeeDto;
 import me.edwardosei.employeeservice.entity.Employee;
 import me.edwardosei.employeeservice.repository.EmployeeRepository;
 import me.edwardosei.employeeservice.service.EmployeeService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,32 +13,19 @@ import org.springframework.stereotype.Service;
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
 
+    private ModelMapper modelMapper;
+
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         // convert employee dto to employee jpa entity
-        Employee employee = new Employee(
-                employeeDto.getId(),
-                employeeDto.getFirstName(),
-                employeeDto.getLastName(),
-                employeeDto.getEmail()
-        );
+        Employee employee = modelMapper.map(employeeDto, Employee.class);
         Employee savedEmployee = employeeRepository.save(employee);
-        return new EmployeeDto(
-                savedEmployee.getId(),
-                savedEmployee.getFirstName(),
-                savedEmployee.getLastName(),
-                savedEmployee.getEmail()
-        );
+        return modelMapper.map(savedEmployee, EmployeeDto.class);
     }
 
     @Override
     public EmployeeDto getEmployeeByEmail(String employeeEmail) {
         Employee employee = employeeRepository.findByEmail(employeeEmail);
-        return new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail()
-        );
+        return modelMapper.map(employee, EmployeeDto.class);
     }
 }
