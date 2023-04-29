d12a185afdd9b6b45977e4d09ea1166d6e476eac
nana-yaw <edward.osei.nyarko@gmail.com>
2023-04-29 15:55:07 +0000

    department mapstruct dependency

---
+++ department-service/src/main/java/me/edwardosei/departmentservice/mapper/AutoDepartmentMapper.java
@@ -0,0 +1,15 @@
+package me.edwardosei.departmentservice.mapper;
+
+import me.edwardosei.departmentservice.dto.DepartmentDto;
+import me.edwardosei.departmentservice.entity.Department;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoDepartmentMapper {
+    AutoDepartmentMapper MAPPER = Mappers.getMapper(AutoDepartmentMapper.class);
+
+    DepartmentDto mapToDepartmentDto(Department department);
+
+    Department mapToDepartment(DepartmentDto departmentDto);
+}
--- department-service/src/main/java/me/edwardosei/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/me/edwardosei/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -3,6 +3,7 @@ package me.edwardosei.departmentservice.service.impl;
 import lombok.AllArgsConstructor;
 import me.edwardosei.departmentservice.dto.DepartmentDto;
 import me.edwardosei.departmentservice.entity.Department;
+import me.edwardosei.departmentservice.mapper.AutoDepartmentMapper;
 import me.edwardosei.departmentservice.repository.DepartmentRepository;
 import me.edwardosei.departmentservice.service.DepartmentService;
 import org.modelmapper.ModelMapper;
@@ -18,15 +19,17 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
         // convert department dto to department jpa entity
-        Department department = modelMapper.map(departmentDto, Department.class);
+//        Department department = modelMapper.map(departmentDto, Department.class);
+        Department department = AutoDepartmentMapper.MAPPER.mapToDepartment(departmentDto);
         Department savedDepartment = departmentRepository.save(department);
         // convert department jpa entity to department dto
-        return modelMapper.map(savedDepartment, DepartmentDto.class);
+//        return modelMapper.map(savedDepartment, DepartmentDto.class);
+        return AutoDepartmentMapper.MAPPER.mapToDepartmentDto(savedDepartment);
     }
 
     @Override
     public DepartmentDto getDepartmentByCode(String departmentCode) {
         Department department = departmentRepository.findByDepartmentCode(departmentCode);
-        return modelMapper.map(department, DepartmentDto.class);
+        return AutoDepartmentMapper.MAPPER.mapToDepartmentDto(department);
     }
 }
