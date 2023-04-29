2c4b108595151b2ed5383722b310df4abb172bf9
nana-yaw <edward.osei.nyarko@gmail.com>
2023-04-29 15:39:48 +0000

    fix:employee model-mapper dependency

--- employee-service/src/main/java/me/edwardosei/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/me/edwardosei/employeeservice/EmployeeServiceApplication.java
@@ -3,9 +3,11 @@ package me.edwardosei.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
+	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
