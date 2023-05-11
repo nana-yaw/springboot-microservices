1c9d3ea43923c3ae5deb514b9b8dff8ff0a48326
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2023-05-11 13:34:36 +0000

    chore: defining general department-service api docs

--- department-service/src/main/java/me/edwardosei/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/me/edwardosei/departmentservice/DepartmentServiceApplication.java
@@ -1,10 +1,35 @@
 package me.edwardosei.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Edward",
+						email = "edward.osei.nyarko@gmail.com",
+						url = "https://github.com/nana-yaw"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/nana-yaw"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Docs",
+				url = "https://github.com/nana-yaw/springboot-microservices/tree/main/department-service"
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 	@Bean
